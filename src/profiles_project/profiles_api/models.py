1435ec0fc5744a2b81e77f82a5864aa5c8a70bb0
Amish Chopra <amish15chopra@gmail.com>
2019-06-21 20:54:24 +0530

    Added permissons and authentication to the UserProfileViewset.

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -33,7 +33,7 @@ class UserProfileManager(BaseUserManager):
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Represents a user profile inside out system."""
+    """Represents a user profile inside our system."""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
